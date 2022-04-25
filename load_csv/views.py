9a1be2409a9e057d12bc1a146d74ff06db22e267
sheready <csstlw@gmail.com>
2022-04-25 16:35:08 +0300

    create a movieviewset to list and retrieve data

--- load_csv/views.py
+++ load_csv/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
+from load_csv.serializers import MovieSerializer
+
+from .models import Movies
 # Create your views here.
+
+class MovieViewSet(viewsets.ViewSet):
+    def list(self, resquest):
+        queryset = Movies.objects.all()
+        serializer = MovieSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    def retrieve(self, request, pk=None):
+        queryset = Movies.objects.all()
+        movie = get_object_or_404(queryset, id=pk)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
